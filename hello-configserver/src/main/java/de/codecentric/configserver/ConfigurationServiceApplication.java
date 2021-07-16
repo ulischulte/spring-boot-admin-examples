f49a0a9d0cb3b9c0b553de02cb4b314e5153001f
Ulrich Schulte <ulrich.schulte@codecentric.de>
2021-07-16 12:02:40 +0200

    added cloud configserver for testing purposes

---
+++ hello-configserver/src/main/java/de/codecentric/configserver/ConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package de.codecentric.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+}
