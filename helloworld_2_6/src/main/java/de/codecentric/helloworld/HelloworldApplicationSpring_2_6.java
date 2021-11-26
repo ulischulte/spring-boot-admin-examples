a0e656dea6964c734871f7a3a6fbc04074dd6ece
Ulrich Schulte <ulrich.schulte@codecentric.de>
2021-11-26 12:33:56 +0100

    add simple Spring Boot 2.6 application for testing purposes

---
+++ helloworld_2_6/src/main/java/de/codecentric/helloworld/HelloworldApplicationSpring_2_6.java
@@ -0,0 +1,13 @@
+package de.codecentric.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplicationSpring_2_6 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplicationSpring_2_6.class, args);
+	}
+
+}
---
+++ helloworld_2_6/src/test/java/de/codecentric/helloworld/HelloworldApplicationSpring26Tests.java
@@ -0,0 +1,13 @@
+package de.codecentric.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationSpring26Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
