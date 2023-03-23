78a3f74a7f5ed73d13bc4a6b04667385a31579a6
ulrichschulte <ulrich.schulte@codecentric.de>
2023-03-23 10:42:33 +0100

    Applied changes to try to reproduce httpexchanges issue described in #2350

--- hello-spring-boot-admin-3-httpexchange/src/main/kotlin/de/codecentric/hellospringbootadmin3/HelloSpringBootAdmin3HttpExchange.kt
+++ hello-spring-boot-admin-3-httpexchange/src/main/kotlin/de/codecentric/hellospringbootadmin3/HelloSpringBootAdmin3HttpExchange.kt
@@ -1,9 +1,11 @@
 package de.codecentric.hellospringbootadmin3
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAdminServer
 class HelloSpringBootAdmin3HttpExchange
 
 fun main(args: Array<String>) {
